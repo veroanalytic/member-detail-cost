5a1d3e0bd05ef89699507dae8bb43baf10715bd6
veroanalytic <veroanalytic@gmail.com>
2022-10-01 10:56:06 -0400

    added min windows function to sql script

--- analysis-script.sql
+++ analysis-script.sql
@@ -4,6 +4,9 @@ USE Repository
 --DELETE FROM Repository.dbo.Member_Detail_Cost
 --WHERE FileBatch_ID = '20220930225257'
 
+SELECT DISTINCT FileBatch_ID
+FROM Repository.dbo.Member_Detail_Cost 
+ORDER BY FileBatch_ID
 
 SELECT TOP 5
 	  Member_ID
@@ -95,7 +98,58 @@ SET @minprice = (SELECT MIN(Cost) FROM Repository.dbo.Member_Detail_Cost )
 SELECT *  
 FROM Repository.dbo.Member_Detail_Cost 
 WHERE Cost = @minprice
+GO
+
+-- Member list of those with only one occurence
+WITH One_Occurrence
+AS 
+(
+SELECT 
+  Member_ID, First_Name, Last_Name, DOB, Service_DT, Cost
+, COUNT(*) OVER (PARTITION BY Member_ID) AS Number_Of_Occurences
+FROM Repository.dbo.Member_Detail_Cost 
+)
+SELECT *
+FROM One_Occurrence AS occ
+WHERE Number_Of_Occurences = 1
+GO
 
 
 
+-- Member list of those with multiple occurences
+WITH One_Occurrence
+AS 
+(
+SELECT 
+  Member_ID, First_Name, Last_Name, DOB, Service_DT, Cost
+, COUNT(*) OVER (PARTITION BY Member_ID) AS Number_Of_Occurences
+FROM Repository.dbo.Member_Detail_Cost 
+)
+SELECT *
+FROM One_Occurrence AS occ
+WHERE Number_Of_Occurences > 1
+ORDER BY Number_Of_Occurences DESC, Member_ID
+GO
 
+
+-- Earliest Service_DT per member, earliest date by product (cost in this case)
+SELECT *
+, MIN(Service_DT) OVER (PARTITION BY Member_ID) AS Earliest_Service_DT
+, MIN(Service_DT) OVER (PARTITION BY Member_ID, Cost) AS Service_DT_By_Cost -- redundant in this dataset with no actual product, but wanted to do it as practice
+FROM Repository.dbo.Member_Detail_Cost 
+ORDER BY Earliest_Service_DT, Member_ID
+GO
+
+
+-- Not really useful in this use case, but if we wanted to track how many times a member was seen after the first time
+WITH After_First_Service AS 
+(
+SELECT *
+, MIN(Service_DT) OVER (PARTITION BY Member_ID) AS Earliest_Service_DT
+, MIN(Service_DT) OVER (PARTITION BY Member_ID, Cost) AS Service_DT_By_Cost -- redundant in this dataset with no actual product, but wanted to do it as practice
+FROM Repository.dbo.Member_Detail_Cost 
+)
+SELECT *
+FROM After_First_Service
+WHERE Earliest_Service_DT < Service_DT_By_Cost  
+ORDER BY Earliest_Service_DT, Member_ID
